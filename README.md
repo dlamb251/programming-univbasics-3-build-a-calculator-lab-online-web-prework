b37cede1daf21054e54ae0768e2c10399677e0b9
Daniel Lamb <daniellamb113@gmail.com>
2019-08-21 02:21:21 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Build a Simple Calculator
+e# Build a Simple Calculator
 
 ## Learning Goals
 
--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+num1+num2
 end
 
 def subtraction(num1, num2)
-
+  
+num1 subtraction num2
 end
 
 def division(num1, num2)
-
+  
+num1 division num2
 end
 
 def multiplication(num1, num2)
-
+  
+num1 multiplication num2
 end
 
 def modulo(num1, num2)
-
+  
+num1 into num2
 end
 
 def square_root(num)
-
+  
+square_root num
 end
+submit_tag
+
