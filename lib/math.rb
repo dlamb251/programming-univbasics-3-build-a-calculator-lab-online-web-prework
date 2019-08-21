58646b7dfc52903576eab95f54958549af7338f0
Daniel Lamb <daniellamb113@gmail.com>
2019-08-21 02:14:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,14 +3,17 @@ num1+num2
 end
 
 def subtraction(num1, num2)
+  
 num2 subtraction num2
 end
 
 def division(num1, num2)
+  
 num1 division num1
 end
 
 def multiplication(num1, num2)
+  
 num2 multiplication num2
 end
 
