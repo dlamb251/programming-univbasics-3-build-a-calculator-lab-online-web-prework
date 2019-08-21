e4f4c6560386431777dccbd041b3a059f7676695
Daniel Lamb <daniellamb113@gmail.com>
2019-08-21 02:16:08 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,24 +4,24 @@ end
 
 def subtraction(num1, num2)
   
-num2 subtraction num2
+num1 subtraction num2
 end
 
 def division(num1, num2)
   
-num1 division num1
+num1 division num2
 end
 
 def multiplication(num1, num2)
   
-num2 multiplication num2
+num1 multiplication num2
 end
 
 def modulo(num1, num2)
-
+num1 into num2
 end
 
 def square_root(num)
-
+square_root num
 end
 submit_tag
